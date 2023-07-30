95367a3d748baa68cdff45f7a786b7e14e7d18c4
Regu-Miabyss <Cyanogenmod.Cid@gmail.com>
2023-07-30 14:47:33 +0800

    Fixed Magisk Module.

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -5,3 +5,6 @@ MODDIR=${0%/*}
 
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
+
+mkdir -p /cache/magisk_mount/system/media/
+cp -f $MODDIR/system/media/bootanimation.zip /cache/magisk_mount/system/media/bootanimation.zip
--- config.sh
+++ config.sh
@@ -10,9 +10,10 @@
 #
 # 1. Place your files into system folder (delete the placeholder file)
 # 2. Fill in your module's info into module.prop
-# 3. Configure the settings in this file (config.sh)
-# 4. If you need boot scripts, add them into common/post-fs-data.sh or common/service.sh
-# 5. Add your additional or modified system properties into common/system.prop
+# 3. Configure the settings in this file (common/config.sh)
+# 4. For advanced features, add shell commands into the script files under common:
+#    post-fs-data.sh, service.sh
+# 5. For changing props, add your additional/modified props into common/system.prop
 #
 ##########################################################################################
 
@@ -28,7 +29,7 @@ AUTOMOUNT=true
 PROPFILE=false
 
 # Set to true if you need post-fs-data script
-POSTFSDATA=false
+POSTFSDATA=true
 
 # Set to true if you need late_start service script
 LATESTARTSERVICE=false
@@ -40,9 +41,10 @@ LATESTARTSERVICE=false
 # Set what you want to show when installing your mod
 
 print_modname() {
+  ui_print "*************************************"
   ui_print "     OneShot Bootanimation     "
   ui_print "          BY:Regu_Miabyss           "
-
+  ui_print "*************************************"
 }
 
 ##########################################################################################
@@ -50,12 +52,21 @@ print_modname() {
 ##########################################################################################
 
 # List all directories you want to directly replace in the system
-# Check the documentations for more info about how Magic Mount works, and why you need this
+# By default Magisk will merge your files with the original system
+# Directories listed here however, will be directly mounted to the correspond directory in the system
 
-# Construct your own list here, it will override the example above
+# You don't need to remove the example below, these values will be overwritten by your own list
+# This is an example
+REPLACE="
+/system/app/Youtube
+/system/priv-app/SystemUI
+/system/priv-app/Settings
+/system/framework
+"
+
+# Construct your own list here, it will overwrite the example
 # !DO NOT! remove this if you don't need to replace anything, leave it empty as it is now
 REPLACE="
-/system/media/bootanimation.zip
 "
 
 ##########################################################################################
@@ -79,14 +90,3 @@ set_permissions() {
   # The following is default permissions, DO NOT remove
   set_perm_recursive  $MODPATH  0  0  0755  0644
 }
-
-##########################################################################################
-# Custom Functions
-##########################################################################################
-
-# This file (config.sh) will be sourced by the main flash script after util_functions.sh
-# If you need custom logic, please add them here as functions, and call these functions in
-# update-binary. Refrain from adding code directly into update-binary, as it will make it
-# difficult for you to migrate your modules to newer template versions.
-# Make update-binary as clean as possible, try to only do function calls in it.
-
